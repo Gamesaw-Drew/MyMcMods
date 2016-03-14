784f361e86ac7d6d146c880e6261772a1d3721a6
DREW <drewgamesaw@gmail.com>
2016-03-14 15:22:57 -0400

    Why was that there?

--- src/main/java/com/drew/drewmod/items/ChipmunkCard.java
+++ src/main/java/com/drew/drewmod/items/ChipmunkCard.java
@@ -33,10 +33,6 @@ public class ChipmunkCard extends Item {
 	
 	@Override
 	public ItemStack onItemRightClick(ItemStack itemstack, World world, EntityPlayer entity){
-		float var4 = 1.0F;
-		int i = (int)(entity.prevPosX + (entity.posX - entity.prevPosX) * (double)var4);
-		int j = (int)(entity.prevPosY + (entity.posY - entity.prevPosY) * (double)var4 + 1.62D);
-		int k = (int)(entity.prevPosZ + (entity.posZ - entity.prevPosZ) * (double)var4);
 
 		if(true){
 		
